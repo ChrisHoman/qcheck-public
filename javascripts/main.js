45d87f1e99fda3a013ed9aab336b8d04f0fe2ebc
Chris Homan <chris@red-tulip.co.uk>
2015-01-20 22:43:36 +0000

    various small

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,56 +1,42 @@
 console.log('This would be the main JS file.');
 
-  WebFontConfig = {
-    google: { families: [ 'Oswald:400,300,700:latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
+// Load webfonts from Google
+WebFontConfig = {
+	google: {
+		families: ['Oswald:400,300,700:latin', 'Open+Sans:300italic,400italic,700italic,400,300,700:latin']
+	}
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
 
-jQuery(function( $ ){
-
+// Parallax effect
+jQuery(function($) {
 	// Enable parallax and fade effects on homepage sections
-	$(window).scroll(function(){
-
+	$(window).scroll(function() {
 		scrolltop = $(window).scrollTop()
 		scrollwindow = scrolltop + $(window).height();
-
-		$(".home-section-1").css("backgroundPosition", "50% " + -(scrolltop/6) + "px");
-
-		if ( $(".home-section-3").length ) {
-		
-			sectionthreeoffset = $(".home-section-3").offset().top;		  
-
-			if( scrollwindow > sectionthreeoffset ) {
-
+		$(".home-section-1").css("backgroundPosition", "50% " + -(scrolltop / 6) + "px");
+		if ($(".home-section-3").length) {
+			sectionthreeoffset = $(".home-section-3").offset().top;
+			if (scrollwindow > sectionthreeoffset) {
 				// Enable parallax effect
 				backgroundscroll = scrollwindow - sectionthreeoffset;
-				$(".home-section-3").css("backgroundPosition", "50% " + -(backgroundscroll/6) + "px");
-
+				$(".home-section-3").css("backgroundPosition", "50% " + -(backgroundscroll / 6) + "px");
 			}
-		
 		}
-
-		if ( $(".home-section-5").length ) {
-		
-			sectionfiveoffset = $(".home-section-5").offset().top;		  
-
-			if( scrollwindow > sectionfiveoffset ) {
-
+		if ($(".home-section-5").length) {
+			sectionfiveoffset = $(".home-section-5").offset().top;
+			if (scrollwindow > sectionfiveoffset) {
 				// Enable parallax effect
 				backgroundscroll = scrollwindow - sectionfiveoffset;
-				$(".home-section-5").css("backgroundPosition", "50% " + -(backgroundscroll/6) + "px");
-
+				$(".home-section-5").css("backgroundPosition", "50% " + -(backgroundscroll / 6) + "px");
 			}
-		
 		}
-
 	});
-
 });
