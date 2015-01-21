1d18ccd1922711cd83bdc29290b15d6f26c9813d
Chris Homan <chris@red-tulip.co.uk>
2015-01-21 15:01:05 +0000

    Various

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,5 @@
 console.log('This would be the main JS file.');
 
-// Load webfonts from Google
-WebFontConfig = {
-	google: {
-		families: ['Oswald:400,300,700:latin', 'Open+Sans:300italic,400italic,700italic,400,300,700:latin']
-	}
-};
-(function() {
-	var wf = document.createElement('script');
-	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-	wf.type = 'text/javascript';
-	wf.async = 'true';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(wf, s);
-})();
-
 // Parallax effect
 jQuery(function($) {
 	// Enable parallax and fade effects on homepage sections
