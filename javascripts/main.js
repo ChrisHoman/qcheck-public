a9e89c5f6083ea1dcd0a36d9a95ec34e9855b45f
Chris Homan <chris@red-tulip.co.uk>
2015-02-25 10:26:43 +0000

    dd

--- javascripts/main.js
+++ javascripts/main.js
@@ -55,7 +55,7 @@ jQuery(function($) {
 	});
 	
 	function resize() {
-		if ($('html').width() > 769 ) {
+		if ($('fieldset').width() > 897 ) {
 			$('form:not(#loginForm) .col-r').css("width",$('.col-r').parent().width() - $('.col-l').width() - 15);
 		}
 		else {
