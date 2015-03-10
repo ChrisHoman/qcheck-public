2c326ba57c7dd4504b6ce840415fdc92e45fa1d5
Chris Homan <chris@red-tulip.co.uk>
2015-03-10 10:44:27 +0000

    dd

--- javascripts/main.js
+++ javascripts/main.js
@@ -62,9 +62,9 @@ jQuery(function($) {
 		$( ".verificationForm #searchName" ).keypress(function (event) {
 			$tooltipText = $('#searchName').attr("title");
 			if ($( ".verificationForm #searchName" ).val().length < 1 ) {
-				$('#searchName').removeAttr('title');
+				$('#searchName').removeAttr( "title" )
 			} else {
-				$('#searchName').attr('title', $tooltipText);
+				$('#searchName').attr('title', $tooltipText)
 			}
 			if ($( ".verificationForm #searchName" ).val().length > 1 ) {
 				$('.verificationForm #searchResults').slideDown( "900", function() {$('.verificationForm #searchResults').css('opacity','1');})
