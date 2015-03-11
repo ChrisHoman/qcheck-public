8dd48e23b4ffa8efaa13d1f1c83633fd1eb45fcb
Chris Homan <chris@red-tulip.co.uk>
2015-03-11 23:58:40 +0000

    ss

--- javascripts/main.js
+++ javascripts/main.js
@@ -36,6 +36,7 @@ jQuery(function($) {
 		$(document).ready(function(){
 			resize();
 			showSearch();
+			equalheight('.ptp-item-container div');
 		});
 		
 		
@@ -67,19 +68,7 @@ jQuery(function($) {
 
 		$(window).on('resize', function(){
 			resize();
-			
-			
-			
-  equalheight('.ptp-row-id-0');
-			
-            
-        
-    });  
-			
-			
-			
-			
-			
+			equalheight('.ptp-item-container div');
 		});
 		
 		function resize() {
@@ -91,35 +80,29 @@ jQuery(function($) {
 		
 		equalheight = function(container){
 
-var currentTallest = 0,
-     currentRowStart = 0,
-     rowDivs = new Array(),
-     $el,
-     topPosition = 0;
- $(container).each(function() {
-
-   $el = $(this);
-   $($el).height('auto')
-   topPostion = $el.position().top;
-
-   if (currentRowStart != topPostion) {
-     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-       rowDivs[currentDiv].height(currentTallest);
-     }
-     rowDivs.length = 0; // empty the array
-     currentRowStart = topPostion;
-     currentTallest = $el.height();
-     rowDivs.push($el);
-   } else {
-     rowDivs.push($el);
-     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-  }
-   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-     rowDivs[currentDiv].height(currentTallest);
-   }
- });
-}
+		 $(container).each(function() {
 		
+		   $el = $(this);
+		   $($el).height('auto')
+		   topPostion = $el.position().top;
+		
+		   if (currentRowStart != topPostion) {
+		     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+		       rowDivs[currentDiv].height(currentTallest);
+		     }
+		     rowDivs.length = 0; // empty the array
+		     currentRowStart = topPostion;
+		     currentTallest = $el.height();
+		     rowDivs.push($el);
+		   } else {
+		     rowDivs.push($el);
+		     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+		  }
+		   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+		     rowDivs[currentDiv].height(currentTallest);
+		   }
+		 });
+		}
 		
 		
 	});
