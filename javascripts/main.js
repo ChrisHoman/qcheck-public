c639c1d0cb79480b5b137b14ef784aa5544c273f
Chris Homan <chris@red-tulip.co.uk>
2015-03-11 23:42:33 +0000

    dd

--- javascripts/main.js
+++ javascripts/main.js
@@ -36,7 +36,6 @@ jQuery(function($) {
 		$(document).ready(function(){
 			resize();
 			showSearch();
-			$('.ptp-row-id-0').eqHeights();
 		});
 		
 		
@@ -68,6 +67,28 @@ jQuery(function($) {
 
 		$(window).on('resize', function(){
 			resize();
+			
+			
+			
+			
+			$('.ptp-row-id-0').each(function(){  
+            
+            var highestBox = 0;
+            $(this).each(function(){
+            
+                if($(this).height() > highestBox) 
+                   highestBox = $(this).height(); 
+            });  
+            
+            $('.ptp-row-id-0').height(highestBox);
+            
+        
+    });  
+			
+			
+			
+			
+			
 		});
 		
 		function resize() {
@@ -75,52 +96,6 @@ jQuery(function($) {
 				$('form:not(#loginForm) .col-r').css("width",$('.col-r').parent().width() - $('.col-l').width() - 15);
 			}
 		}
-		
-		
-		
-		$.fn.eqHeights = function(options) {
-
-		    var defaults = {  
-		        child: false 
-		    };  
-		    var options = $.extend(defaults, options); 
-		
-		    var el = $(this);
-		    if (el.length > 0 && !el.data('eqHeights')) {
-		        $(window).bind('resize.eqHeights', function() {
-		            el.eqHeights();
-		        });
-		        el.data('eqHeights', true);
-		    }
-		
-		    if( options.child && options.child.length > 0 ){
-		        var elmtns = $(options.child, this);
-		    } else {
-		        var elmtns = $(this).children();
-		    }
-		
-		    var prevTop = 0;
-		    var max_height = 0;
-		    var elements = [];
-		    elmtns.height('auto').each(function() {
-		
-		        var thisTop = this.offsetTop;
-		
-		        if (prevTop > 0 && prevTop != thisTop) {
-		            $(elements).height(max_height);
-		            max_height = $(this).height();
-		            elements = [];
-		        }
-		        max_height = Math.max(max_height, $(this).height());
-		
-		        prevTop = this.offsetTop;
-		        elements.push(this);
-		    });
-		
-		    $(elements).height(max_height);
-		};
-		
-		
 		
 	});
 });
