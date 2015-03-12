ca8353f88936f5e5b79a8a7bba9742e48cb0370c
Chris Homan <chris@red-tulip.co.uk>
2015-03-12 00:11:21 +0000

    h

--- javascripts/main.js
+++ javascripts/main.js
@@ -36,7 +36,7 @@ jQuery(function($) {
 		$(document).ready(function(){
 			resize();
 			showSearch();
-			$('.ptp-row-id-0').equalHeights();
+			equalise();
 		});
 		
 		
@@ -77,18 +77,14 @@ jQuery(function($) {
 		}
 		
 		
-$.fn.equalHeights = function(px) {
+function equalise() {
 	$(this).each(function(){
 		var currentTallest = 0;
-		$(this).children().each(function(i){
+		$('.ptp-row-id-0').each(function(){
 			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
 		});
-    if (!px && Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
-		// for ie6, set height since min-height isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'height': currentTallest}); }
-		$(this).children().css({'min-height': currentTallest}); 
 	});
-	return this;
+	$('.ptp-row-id-0').height(currentTallest);
 };
 		
 		
