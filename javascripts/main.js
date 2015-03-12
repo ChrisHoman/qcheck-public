c2072fd47ce86c32261f11b6b48d6329e4ceedb8
Chris Homan <chris@red-tulip.co.uk>
2015-03-12 01:21:39 +0000

    cc

--- javascripts/main.js
+++ javascripts/main.js
@@ -86,39 +86,34 @@ jQuery(function($) {
 			}
 		}
 		
-/*
-
-		equalheight = function(container){
-*/
-		
 		function equalheight(container) {
-		var currentTallest = 0,
-		     currentRowStart = 0,
-		     rowDivs = new Array(),
-		     $el,
-		     topPosition = 0;
-		 $(container).each(function() {
-		
-		   $el = $(this);
-		   $($el).height('auto')
-		   topPostion = $el.position().top;
-		
-		   if (currentRowStart != topPostion) {
-		     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-		       rowDivs[currentDiv].height(currentTallest);
-		     }
-		     rowDivs.length = 0; // empty the array
-		     currentRowStart = topPostion;
-		     currentTallest = $el.height();
-		     rowDivs.push($el);
-		   } else {
-		     rowDivs.push($el);
-		     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-		  }
-		   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-		     rowDivs[currentDiv].height(currentTallest);
-		   }
-		 });
+			var currentTallest = 0,
+				currentRowStart = 0,
+				rowDivs = new Array(),
+				$el,
+				topPosition = 0;
+			$(container).each(function() {
+			
+				$el = $(this);
+				$($el).height('auto')
+				topPostion = $el.position().top;
+				
+				if (currentRowStart != topPostion) {
+					for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+						rowDivs[currentDiv].height(currentTallest);
+					}
+					rowDivs.length = 0; // empty the array
+					currentRowStart = topPostion;
+					currentTallest = $el.height();
+					rowDivs.push($el);
+				} else {
+					rowDivs.push($el);
+					currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+				}
+				for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+					rowDivs[currentDiv].height(currentTallest);
+				}
+			});
 		}
 
 		
